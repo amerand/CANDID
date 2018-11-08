6b718f9ee471d0bc331c95d6e6c55bf452291446
amerand <amerand@eso.org>
2018-11-08 08:37:53 +0100

    version 1.0 for Python 3

--- axcir.py
+++ axcir.py
@@ -4,24 +4,20 @@ from matplotlib import pyplot as plt
 def runAll(savefig=False):
     global o
     o = candid.Open('AXCir.oifits')
+
     o.chi2Map(fig=1, fratio=1.0)
-    if savefig:
-        plt.savefig('doc/figure_1.png')
 
     o.fitMap(fig=2)
-    if savefig:
-        plt.savefig('doc/figure_2.png')
 
     o.fitBoot(fig=3)
-    if savefig:
-        plt.savefig('doc/figure_3.png')
 
     p = o.bestFit['best']
     o.fitMap(fig=4, removeCompanion=p)
-    if savefig:
-        plt.savefig('doc/figure_4.png')
 
     o.detectionLimit(fig=5, step=1.5, removeCompanion=p)
+
     if savefig:
-        plt.savefig('doc/figure_5.png')
+        for f in [1,2,3,4,5]:
+            plt.figure(f)
+            plt.savefig('doc/figure_%d.png'%f)
     return
