f094577819924f7293daa4ad2f50ffe6b951a104
Antoine Mérand <antoine.merand@gmail.com>
2023-07-20 11:37:00 +0200

    convert to new setuptools

--- candid/demo/axcir.py
+++ candid/demo/axcir.py
@@ -1,9 +1,11 @@
 import candid
 from matplotlib import pyplot as plt
+import os
 
 def runAll(savefig=False):
     global o
-    o = candid.Open('AXCir.oifits')
+    directory = os.path.join(os.path.dirname(candid.__file__), 'demo')
+    o = candid.Open(os.path.join(directory, 'AXCir.oifits'))
 
     o.chi2Map(fig=1, fratio=1.0)
 
@@ -15,6 +17,8 @@ def runAll(savefig=False):
     o.fitMap(fig=4, removeCompanion=p)
 
     o.detectionLimit(fig=5, step=1.5, removeCompanion=p)
+
+    plt.show()
 
     if savefig:
         for f in [1,2,3,4,5]:
