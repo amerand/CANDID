55ae76291365400f6751a8ecae5277a7eeea9c8d
Antoine Mérand <antoine.merand@gmail.com>
2023-07-20 12:20:17 +0200

    convert to new setuptools

--- candid/demo/axcir.py
+++ candid/demo/axcir.py
@@ -1,9 +1,14 @@
 import candid
 from matplotlib import pyplot as plt
 import os
+import time
+
+
+candid.CONFIG['long exec warning'] = None
 
 def runAll(savefig=False):
     global o
+    t0 = time.time()
     directory = os.path.join(os.path.dirname(candid.__file__), 'demo')
     o = candid.Open(os.path.join(directory, 'AXCir.oifits'))
 
@@ -17,8 +22,10 @@ def runAll(savefig=False):
     o.fitMap(fig=4, removeCompanion=p)
 
     o.detectionLimit(fig=5, step=1.5, removeCompanion=p)
-
     plt.show()
+    print('#'*20)
+    print('done in %.0f'%(time.time()-t0))
+    print('#'*20)
 
     if savefig:
         for f in [1,2,3,4,5]:
